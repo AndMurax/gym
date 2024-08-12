78c1a874ceab6b343f95df058de599af4fccd4b2
Anderson Conceicao <anderson.conceicao@prodeb.ba.gov.br>
2024-08-12 16:59:44 -0300

    (feat): Ocultando o valor do Dashboard

--- application/views/pages/Dashboard.php
+++ application/views/pages/Dashboard.php
@@ -1,4 +1,36 @@
+    <style>
+        .ocultar {
+            display: none;
+            }
+    </style>
 
+    <script>
+        function ValorAnual() {
+            var valorAnual = document.getElementById("valor-anual");
+            var toggleButton = document.getElementById("toggle-button");
+
+            if (valorAnual.style.display === "none") {
+                valorAnual.style.display = "block";
+                toggleButton.textContent = "Ocultar Valor";
+            } else {
+                valorAnual.style.display = "none";
+                toggleButton.textContent = "Mostrar Valor";
+            }
+        }
+
+        function ValorMensal() {
+            var valorMensal = document.getElementById("valor-mensal");
+            var toggleButton = document.getElementById("toggle-button");
+
+            if (valorMensal.style.display === "none") {
+                valorMensal.style.display = "block";
+                toggleButton.textContent = "Ocultar Valor";
+            } else {
+                valorMensal.style.display = "none";
+                toggleButton.textContent = "Mostrar Valor";
+            }
+        }
+    </script>
 	<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
 	<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
 		<h1 class="h2">Dashboard</h1>
@@ -13,38 +45,46 @@
 	<div class="row"> 
 <!-- Earnings (Monthly) Card Example -->
 <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-primary shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                Ganhos (Mensais)</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">R$: <?= $valor_mensal['total']?></div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                    Ganhos (Mensais)
+                                </div>
+                                <div id="valor-mensal" class="h5 mb-0 font-weight-bold text-gray-800" style="display: none;">
+                                    R$: <?= $valor_mensal['total'] ?>
                                 </div>
                             </div>
+                            <div class="col-auto">
+                                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                            </div>
                         </div>
+                        <button id="toggle-button" class="btn btn-primary mt-3" onclick="ValorMensal()">Mostrar Valor</button>
+                    </div>
+                </div>
+            </div>
 <!-- Earnings (Anual) Card Example -->						
-						<div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-success shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                Ganhos (Anual)</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">R$: <?= $valor_anual['total']?></div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-success shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                    Ganhos (Anual)
+                                </div>
+                                <div id="valor-anual" class="h5 mb-0 font-weight-bold text-gray-800" style="display: none;">
+                                    R$: <?= $valor_anual['total'] ?>
                                 </div>
                             </div>
+                            <div class="col-auto">
+                                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                            </div>
                         </div>
+                        <button id="toggle-button" class="btn btn-primary mt-3" onclick="ValorAnual()">Mostrar Valor</button>
+                    </div>
+                </div>
+            </div>
 
 
 	    <!-- Earnings (Membros ativos) Card Example -->
